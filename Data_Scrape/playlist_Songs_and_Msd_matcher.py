dacbb0ce7b955eaee3d2b0c2095d1a37b7e4641e
ArkaSarkar19 <arka18222@iiitd.ac.in>
2020-09-24 04:09:56 +0530

    added spotify playlsit and msd playlist intersection

---
+++ Data_Scrape/playlist_Songs_and_Msd_matcher.py
@@ -0,0 +1,40 @@
+import spotipy 
+import os
+import pickle
+from spotipy.oauth2 import SpotifyClientCredentials
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="89e3323f79474b3a9de0caf291ba18de",
+                                                           client_secret="d0a84797538d4c8c86eed735569a24a5"))
+f = open("playlist_names.txt" ,"r")
+dict_ = open("Msd_id_to_spotify_id.pkl", "rb")
+dict_ = pickle.load(dict_)
+dict_ = dict(map(reversed, dict_.items()))
+
+print(len(dict_))
+links_list1 = []
+
+error = []
+count  = 0
+for line in f:
+	playlist_name = line[:-1]
+	results = sp.search(q=playlist_name, type='playlist', limit = 1)
+	try :
+		playlist_id = results['playlists']['items'][0]['id']
+		results = sp.playlist_items(playlist_id, limit = 100)
+		# if(i == 1):
+			# break
+		# print(results['items'][0]['track']['id'])
+		for i in range(len(results['items'])):
+			# print(results['items'][i]['track']['id'], end = " ")
+
+			song_id = results['items'][i]['track']['id']
+			if(song_id in dict_):
+				count+=1
+		print(playlist_name, count,len(results['items']) )
+		count = 0
+		# print("\n\n")
+	except:
+		# print("Error ")
+		# print(playlist_name)
+		error.append(playlist_name)
+print(error)
