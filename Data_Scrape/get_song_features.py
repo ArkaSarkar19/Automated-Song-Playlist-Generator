24db8d7236ca2e5383eb27a6d99a951c98d190ab
pankilkalra <pankil18061@iiitd.ac.in>
2020-09-26 00:19:50 +0530

    extracted relevant song ids

--- Data_Scrape/get_song_features.py
+++ Data_Scrape/get_song_features.py
@@ -4,30 +4,21 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="f053486a0d364fe09c5bddf49fc4657e",
                                                            client_secret="701cacd9264046d7889ab46f6f7610a9"))
-f = open("playlist_names.txt" ,"r")
+f = open("playlist_ids.txt" ,"r")
+
+playlist_list = []
+for line in f:
+	playlist_id, x, y = line.split() 
+	playlist_list.append(playlist_id)
+f.close()
 
 f = open("playlist_names.txt" ,"r")
 dict_ = open("Msd_id_to_spotify_id.pkl", "rb")
 dict_ = pickle.load(dict_)
 dict_ = dict(map(reversed, dict_.items()))
+f.close()
 
-playlist_list = []
-bias = 0
-for i in range(10):
-	results = sp.search(q="latin", type='playlist', limit = 50, offset = bias)
-	bias += 50
-	for j in range(len(results['playlists']['items'])):
-		playlist_id = results['playlists']['items'][j]['id']
-		playlist_list.append(playlist_id)
-
-s = set()
-for c in playlist_list:
-	s.add(c)
-
-playlist_list = list(s)
-
-print(len(playlist_list))
-
+song_ids = set()
 
 for playlist_id in playlist_list:		
 	offset = 0
@@ -41,11 +32,21 @@ for playlist_id in playlist_list:
 				song_id = results1['items'][j]['track']['id']
 				if(song_id in dict_):
 					count+=1
+					song_ids.add(song_id)
 			except:
 				continue
 		offset += len(results1['items'])
+	print(playlist_id, count,offset)
+
+list1 = list(song_ids)
+print(list1)
+print(len(list1))
+
+f = open("relevant_songs.pkl", "wb")
+pickle.dump(list1, f)
+f.close()
 	
-# 	# print(results['playlists']['items'][i]['name'])
-	if count > 50:
-		print(playlist_id, count,offset)
+# # 	# print(results['playlists']['items'][i]['name'])
+# 	if count > 50:
+		# print(playlist_id, count,offset)
 
